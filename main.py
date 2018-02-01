898f64f259a6307045d4efb46439d00005ffa7e1
Rajat Bansal <rajatbansal.425@gmail.com>
2018-02-01 17:53:34 +0530

    Validations and additional Informations for profile have been added

--- main.py
+++ main.py
@@ -1,4 +1,56 @@
 print 'Welcome!'
+
+#Asking Name of the Spy
 spy_name = raw_input("Please Enter Your Name:")
-spy_salut = raw_input("What should I call you Mr. or Mrs.? :")
-print 'Welcome' + ' ' + spy_salut + spy_name + ' ' + 'now you are a member of SpyChat carry on and take benifits of being a spy.'
+
+#Validation on Name
+if spy_name > 3:
+    spy_salute = raw_input("What should I call you Mr. or Mrs.? :")
+
+    #Validation on Salutation
+    if spy_salute == 'Mr.' or spy_salute == 'Mrs.' or spy_salute == 'mr.' or spy_salute == 'mrs.':
+        print 'Welcome' + ' ' + spy_salute + spy_name + ' ' + 'Please give us some additional information about yourself.'
+
+        # declaration of New variables
+        spy_age = 0
+        spy_rating = 0.0
+        spy_online = False
+
+        # Input Age
+        spy_age = raw_input("What is your age:")
+
+        # Age Validations
+        if spy_age >= '16' and spy_age <= '50' :
+            print 'Great!' + ' ' + spy_salute + spy_name + ' ' + 'You look perfectly eligible to become a SpyChat user.'
+
+            # input Ratings
+            spy_rating = raw_input('Please enter your rating as a spy (Max 5.0):')
+
+            # Rating Validations
+            if spy_rating > '0.0' and spy_rating <= '5.0' :
+                if spy_rating >= '3.0' and spy_rating <= '4.5' :
+                    print 'Cool ! It seems you are a Good Spy.'
+                elif spy_rating >= '4.5':
+                    print 'Wow ! You are brilliant.'
+                else:
+                    print 'It seems you need more experience.'
+
+                # Successful Registration and Printing Entered Informations
+                print 'Great!' + ' ' + spy_salute + spy_name + ' ' + 'You have successfully registered to SpyChat.'
+                print 'Here are the Informations provided by you as below:'
+                print 'Name :' + ' ' + spy_salute + spy_name
+                print 'Age :' + ' ' + str(spy_age)
+                print 'Rating :' + ' ' + str(spy_rating)
+                spy_online = True
+
+            else:
+                print 'It seems you have entered a wrong rating please try again.'
+
+        else:
+            print 'I am Sorry !' + ' ' + spy_salute + spy_name + ' ' + 'You seems to be not eligible to become a SpyChat User.'
+
+    else:
+        print ' Please enter a valid salutation from given options ! '
+
+else:
+    print 'Please enter a valid name with atleast 3 characters'
