a96e88779296d002a907a6b03edf453aed8bf8ed
Rajat Bansal <rajatbansal.425@gmail.com>
2018-02-01 14:35:26 +0530

    Basic version

---
+++ main.py
@@ -0,0 +1,4 @@
+print 'Welcome!'
+spy_name = raw_input("Please Enter Your Name:")
+spy_salut = raw_input("What should I call you Mr. or Mrs.? :")
+print 'Welcome' + ' ' + spy_salut + spy_name + ' ' + 'now you are a member of SpyChat carry on and take benifits of being a spy.'
