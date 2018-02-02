5ddfe1b4ac2ecfb83e5531f5becf25174fb02758
Rajat Bansal <rajatbansal.425@gmail.com>
2018-02-03 00:02:25 +0530

    Sign up,log in fuctions added and imformation for existing user is fetched from spydetail

--- main.py
+++ main.py
@@ -1,63 +1,98 @@
-print 'Welcome!'
+import spydetail
+print 'Welcome to SpyChat'
 
-#Asking Name of the Spy
-spy_name = raw_input("Please Enter Your Name:")
+#signup function
+def signup():
+    print 'Sign Up'
+    #Asking Name of the Spy
+    spy_name = raw_input("Please Enter Your Name:")
 
-#Validation on Name
-if len(spy_name) > 0:
-    spy_salute = raw_input("What should I call you Mr. or Mrs.? :")
+    #Validation on Name
+    if len(spy_name) > 0:
+        spy_salute = raw_input("What should I call you Mr. or Mrs.? :")
 
-    #Validation on Salutation
-    if spy_salute == 'Mr.' or spy_salute == 'Mrs.' or spy_salute == 'mr.' or spy_salute == 'mrs.':
-        print 'Welcome' + ' ' + spy_salute + spy_name + ' ' + 'Please give us some additional information about yourself.'
+        #Validation on Salutation
+        if spy_salute == 'Mr.' or spy_salute == 'Mrs.' or spy_salute == 'mr.' or spy_salute == 'mrs.':
+            print 'Welcome' + ' ' + spy_salute + spy_name + ' ' + 'Please give us some additional information about yourself.'
 
-        # declaration of New variables
-        spy_age = 0
-        spy_rating = 0.0
-        spy_online = False
+            # declaration of New variables
+            spy_age = 0
+            spy_rating = 0.0
+            spy_online = False
 
-        # Input Age
-        spy_age = raw_input("What is your age:")
+            #Asking for password
+            spy_password = raw_input("Enter your Password:")
 
-        # Age Validations
-        if spy_age >= '12' and spy_age <= '50' :
-            print 'Great!' + ' ' + spy_salute + spy_name + ' ' + 'You look perfectly eligible to become a SpyChat user.'
+            # Input Age
+            spy_age = raw_input("What is your age:")
 
-            # input Ratings
-            spy_rating = raw_input('Please enter your rating as a spy (Max 5.0):')
+            # Age Validations
+            if spy_age >= '12' and spy_age <= '50' :
+                print 'Great!' + ' ' + spy_salute + spy_name + ' ' + 'You look perfectly eligible to become a SpyChat user.'
 
-            # Rating Validations
-            if spy_rating > '0.0' and spy_rating <= '5.0' :
-                if spy_rating >= '3.0' and spy_rating <= '4.5' :
-                    print 'Cool ! It seems you are a Good Spy.'
-                elif spy_rating >= '4.5':
-                    print 'Wow ! You are brilliant.'
+                # input Ratings
+                spy_rating = raw_input('Please enter your rating as a spy (Max 5.0):')
+
+                # Rating Validations
+                if spy_rating > '0.0' and spy_rating <= '5.0' :
+                    if spy_rating >= '3.0' and spy_rating <= '4.5' :
+                        print 'Cool ! It seems you are a Good Spy.'
+                    elif spy_rating >= '4.5':
+                        print 'Wow ! You are brilliant.'
+                    else:
+                        print 'It seems you need more experience.'
+
+                    # Successful Registration and Printing Entered Informations
+                    print 'Great!' + ' ' + spy_salute + spy_name + ' ' + 'You have successfully registered to SpyChat.'
+                    print 'Here are the Informations provided by you as below:'
+                    print 'Name :' + ' ' + spy_salute + spy_name
+                    print 'Age :' + ' ' + str(spy_age)
+                    print 'Rating :' + ' ' + str(spy_rating)
+                    login()
+
                 else:
-                    print 'It seems you need more experience.'
-
-                # Successful Registration and Printing Entered Informations
-                print 'Great!' + ' ' + spy_salute + spy_name + ' ' + 'You have successfully registered to SpyChat.'
-                print 'Here are the Informations provided by you as below:'
-                print 'Name :' + ' ' + spy_salute + spy_name
-                print 'Age :' + ' ' + str(spy_age)
-                print 'Rating :' + ' ' + str(spy_rating)
-                spy_online = True
+                    print 'It seems you have entered a wrong rating please try again.'
+                #Rating Validations End
 
             else:
-                print 'It seems you have entered a wrong rating please try again.'
-            #Rating Validations End
+                print 'I am Sorry !' + ' ' + spy_salute + spy_name + ' ' + 'You seems to be not eligible to become a SpyChat User.'
+            # Age Validation Ends
+
+        elif spy_salute == 'Mr' or spy_salute == 'Mrs' or spy_salute == 'mr' or spy_salute == 'mrs' :
+            print 'Please enter dot(.) after salutation'
 
         else:
-            print 'I am Sorry !' + ' ' + spy_salute + spy_name + ' ' + 'You seems to be not eligible to become a SpyChat User.'
-        # Age Validation Ends
-
-    elif spy_salute == 'Mr' or spy_salute == 'Mrs' or spy_salute == 'mr' or spy_salute == 'mrs' :
-        print 'Please enter dot(.) after salutation'
+            print ' Please enter a valid salutation from given options ! '
+        #Salutation Validation Ends
 
     else:
-        print ' Please enter a valid salutation from given options ! '
-    #Salutation Validation Ends
+        print 'Please enter a valid name with atleast 3 characters'
+    #Name validation Ends
 
-else:
-    print 'Please enter a valid name with atleast 3 characters'
-#Name validation Ends
+#Login Function
+def login():
+    print 'Log In'
+    spy_name = raw_input("Please enter your Name:")
+    spy_password = raw_input("Please enter your password:")
+    if spy_name == spydetail.spy_name and spy_password == spydetail.spy_password :
+        print 'You are Authenticated'
+        return 1
+    else :
+        print 'Your Provided Informations are not found in our database please sign Up.'
+        return 0
+
+#Introduction
+while True :
+    spy_op = 0
+    success = 0
+    spy_op =  input('Please Log In or Sign Up to Continue \n 1. Log In \n 2. Sign Up\n 3.Exit \n')
+    if spy_op == 1:
+        success = login()
+        if success == 1:
+            break
+    elif spy_op == 2:
+        signup()
+    elif spy_op == 3:
+        break
+    else:
+        print 'You Have Entered a wrong choice try again!'
---
+++ spydetail.py
@@ -0,0 +1,4 @@
+spy_name = 'Mr.Rajat Bansal'
+spy_password = '2march93'
+spy_rating = '5.0'
+spy_age = '23'
