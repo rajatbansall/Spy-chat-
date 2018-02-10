914a9b531d3cdc35dd3c52d7db065eb0103531ca
Rajat Bansal <rajatbansal.425@gmail.com>
2018-02-10 14:23:22 +0530

    Added Status update and Adding a friend module with validations

--- main.py
+++ main.py
@@ -1,26 +1,127 @@
 import spydetail
 print 'Welcome to SpyChat'
 
+#Adding a Friend
+def add_friend():
+    i = 0
+    j = 1
+
+    friend_name = raw_input("Please Enter your friend's name :")
+    # Validation on Name
+    if len(friend_name) > 0:
+        friend_salut = raw_input("What should I call him/her Mr. or Mrs.? :")
+
+        # Validation on Salutation
+        if friend_salut == 'Mr.' or friend_salut == 'Mrs.' or friend_salut == 'mr.' or friend_salut == 'mrs.':
+            # declaration of New variables
+            friend_age = 0
+            friend_rating = 0.0
+
+            # Input Age
+            friend_age = raw_input("What is your friend's age:")
+
+            # Age Validations
+            if friend_age >= '12' and friend_age <= '50':
+                # input Ratings
+                friend_rating = raw_input('Please enter your friend\'s rating(Max 5.0):')
+
+                # Rating Validations
+                if friend_rating > '0.0' and friend_rating <= '5.0':
+                    # Successful Registration and Printing Entered Informations
+                    print 'Great! your friend has been added successfully'
+                    friend_name = friend_salut + friend_name
+                    spydetail.spy_friend['friend_name'] = friend_name
+                    spydetail.spy_friend['friend_age'] = friend_age
+                    spydetail.spy_friend['friend_rating'] = friend_rating
+                    print 'Here are the Informations provided by you as below:'
+                    #printing friend
+                    for friend in spydetail.spy_friend.items():
+                        print str(j) + " " + friend[0] + ": " + str(friend[1])
+                        i = i + 1
+                        j = j + 1
+                    return
+                else:
+                    print 'It seems you have entered a wrong rating please try again.'
+                # Rating Validations End
+
+            else:
+                print 'I am Sorry !' + ' ' + friend_salut + friend_name + ' ' + 'seems not to be eligible.'
+            # Age Validation Ends
+
+        elif friend_salut == 'Mr' or friend_salut == 'Mrs' or friend_salut == 'mr' or friend_salut == 'mrs':
+            print 'Please enter dot(.) after salutation'
+
+        else:
+            print ' Please enter a valid salutation from given options ! '
+        # Salutation Validation Ends
+
+    else:
+        print 'Please enter a valid name with atleast 3 characters'
+    # Name validation Ends
+
+
+#Status Update
+def status():
+    j = 1
+
+    #Displaying Current Status
+    print 'your current status is : ' +  spydetail.spy_user['spy_status']
+
+    stat_op = input("Do you want to choose old status or create new ? /n 1.Old Status 2.Create New\n")
+
+    #Printing and asking the custom status that user want to set
+    if stat_op == 1:
+        print 'Please select a Status from the given list :\n'
+        for  i in spydetail.custom_stat :
+            print str(j) + ' ' + i
+            j = j + 1
+        stat_choice = input() - 1
+        spydetail.spy_user['spy_status'] = spydetail.custom_stat[stat_choice]
+        return 1
+
+    #Setting new status
+    elif stat_op == 2:
+        new_status = raw_input('Please Enter your new status :')
+        spydetail.custom_stat.append((new_status))
+        spydetail.spy_user['spy_status'] = new_status
+        return 1
+    else :
+        return 0
+
 #Menu
 def menu() :
     spy_choice = input("Welcome to Spy chat\n What tasks you want to do ? \n 1.Add a Frnd\n 2.Send Message \n 3. Status Update\n 4. Log Out")
-    return spy_choice
+    success = task(spy_choice)
+    if success == 0 :
+        return
 
 def task(choice) :
+    success = 0
     if choice == 1:
         # Add a Friend
-        print 'Friend has been added'
+        add_friend()
+        menu()
+
     elif choice == 2:
         # Send a Message
         print 'Message has been sent'
+
     elif choice == 3:
         # Status Update
-        print 'Status has been set'
+        success = status()
+        if success == 1 :
+            print 'Status has been set'
+            print 'Your current status has been set to : ' + spydetail.spy_user['spy_status']
+            menu()
+        else :
+            print 'There was some error in updating your status please try again !'
+            menu()
+
     elif choice == 4:
-        return 4
+        return 0
     else:
         print 'Maybe you have entered an invalid choice please try again'
-        return 0
+        menu()
 
 #signup function
 def signup():
@@ -95,7 +196,7 @@ def login():
     print 'Log In'
     spy_name = raw_input("Please enter your Name:")
     spy_password = raw_input("Please enter your password:")
-    if spy_name == spydetail.spy_name and spy_password == spydetail.spy_password :
+    if spy_name == spydetail.spy_user['spy_name'] and spy_password == spydetail.spy_user['spy_password'] :
         print 'You are Authenticated'
         return 1
     else :
@@ -106,17 +207,15 @@ def login():
 while True :
     spy_op = 0
     success = 0
-    choice = 0
-    #Asking to Log in or Sign up
-    spy_op =  input('Please Log In or Sign Up to Continue \n 1. Log In \n 2. Sign Up\n 3.Exit \n')
+    #Asking fro being a default user or new
+    spy_op =  input('Are You an Existing User or New User? \n 1. Existing User  \n 2. New User\n 3.Exit \n')
     if spy_op == 1:
+        print 'We have your information stored.'
         success = login()
         if success == 1:
             while True :
-                choice = menu()
-                choice = task(choice)
-                if choice == 4 :
-                    break
+                menu()
+                break
     elif spy_op == 2:
         signup()
     elif spy_op == 3:
--- spydetail.py
+++ spydetail.py
@@ -1,4 +1,3 @@
-spy_name = 'Mr.Rajat Bansal'
-spy_password = '2march93'
-spy_rating = '5.0'
-spy_age = '23'
+spy_user = {'spy_name' : 'Mr.Rajat Bansal' , 'spy_password' : '2march93', 'spy_rating' : '5.0', 'spy_age' : '23', 'spy_status': 'None'}
+custom_stat = ["Can't talk, Spychat only", "Available", "At Work", "In a Meeting", "Sleeping"]
+spy_friend = {'friend_name' : '', 'friend_age' : 0, 'friend_rating' : 0.0 }
